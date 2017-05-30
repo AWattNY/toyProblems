22df59b03f923518ae1babdd209cee356e22f5c1
Adam Watt <awattny@me.com>
2017-05-29 21:45:51 -0700

    ES6 Classes and subClasses

---
+++ classes.js
@@ -0,0 +1,18 @@
+class Car {
+  constructor ({ title }) {
+    this.title = title;
+  }
+  drive() {
+    return 'vroom';
+  }
+}
+
+class Toyota extends Car {
+  constructor(options) {
+    super(options);
+    this.color = options.color;
+  }
+  honk() {
+    return 'beep';
+  }
+}
