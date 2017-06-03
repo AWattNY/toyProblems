d1a25b24c1228a7e663e785d647380355e333a83
Adam Watt <awattny@me.com>
2017-06-02 20:04:02 -0700

    complete longest increasing subsequence

---
+++ lis.js
@@ -0,0 +1,20 @@
+//Longest Increasing Subseuence
+
+const lengthOfLIS = ( array ) => {
+  const length = array.length;
+  let lis = array.map(e => 1);
+  let max = 0;
+  for ( var i = 0; i < length; i++ ) {
+    for ( var j = 0; j < i; j++ ) {
+      if ( array[i] > array [j] ) {
+        lis[i] = Math.max( lis[j] + 1, lis[i] );
+      }
+    }
+    max = Math.max(lis[i], max);
+  }
+  return max;
+};
+
+const myArray = [10, 22, 9, 33, 21, 50, 41, 60];
+
+console.log(lengthOfLIS(myArray));
