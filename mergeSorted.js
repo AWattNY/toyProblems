ad9a0016c92c3bbb96b331eb751958045a2f4187
Adam Watt <awattny@me.com>
2017-06-11 20:16:21 -0700

    implement mergeSorted

---
+++ mergeSorted.js
@@ -0,0 +1,22 @@
+const sortedMerge = ( arrA, arrB ) => {
+  let lastA = arrA.length - 1;
+  let lastB = arrB.length - 1;
+  let mergeIndex = lastA + lastB + 1;
+
+  while ( lastB >= 0 ) {
+    if ( lastA >= 0 && arrB[lastB] >= arrA[lastA]) {
+      arrA[mergeIndex] = arrB[lastB];
+      lastB--;  
+    } else {
+      arrA[mergeIndex] = arrA[lastA];
+      lastA--;
+    }
+    mergeIndex--;
+  }
+};
+
+// var A = [1, 5, 8, 12];
+
+// var B = [3, 7, 10, 22];
+// sortedMerge(A, B);
+// console.log(A);
