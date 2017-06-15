04d2b117ed49bac877a3c3a1e7856d82f9c2e835
Adam Watt <awattny@me.com>
2017-06-15 07:59:12 -0700

    implement get Anagrams

---
+++ getAnagrams.js
@@ -0,0 +1,17 @@
+const getAllAnagrams = (string) => {
+  let hashMap = new Map();
+  (function getAnagrams( anagram, str ) {
+    if ( str === '' ) {
+      hashMap.set(anagram, anagram);
+    }
+    for ( var i = 0; i < str.length; i++ ) {
+      getAnagrams(anagram + str.charAt(i), str.slice(0, i) + str.slice(i + 1));
+    }
+  })('', string);
+  return hashMap.keys(); 
+};
+
+
+
+
+console.log(getAllAnagrams('abc'));
