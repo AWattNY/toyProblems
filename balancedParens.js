eeea20af0ef519325a270588f6813bd02806efe5
Adam Watt <awattny@me.com>
2017-06-15 07:57:54 -0700

    implement balancd Parens

---
+++ balancedParens.js
@@ -0,0 +1,23 @@
+const balancedParens = (string) => {
+  const pairs = {
+    '{': '}',
+    '[': ']',
+    '(': ')'
+  };  
+
+  let stack = [];
+  for ( var i = 0; i < string.length; i++ ) {
+    var char = string.charAt(i);
+    if ( pairs[char] ) {
+      stack.push(pairs[char]);
+    } else if ( char === '}' || char === ']' || char === ')') {
+      if ( char !== stack.pop() ) {
+        return false;
+      }
+    }  
+  }
+
+  return stack.length === 0;
+};
+
+console.log(balancedParens('[(]{)}'));
