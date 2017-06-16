e3501212f5db98d351d6a26ead76a98196db911b
Adam Watt <awattny@me.com>
2017-06-16 11:39:36 -0700

    implement searchRotated without rotation

--- searchRotated.js
+++ searchRotated.js
@@ -1,26 +1,30 @@
-const searchRotated = (Arr, target, start, end) => {
-  let mid = Math.floor(( start + end ) / 2);
+const searchRotated = (array, target) => {
+  let start = 0;
+  let end = array.length - 1;
+  while ( start <= end ) {
+    let mid = Math.floor(( start + end ) / 2);
   
-  if ( Arr[mid] === target ) {
-    return mid;
-  }
+    if ( array[mid] === target ) {
+      return mid;
+    }
 
-  if ( Arr[mid] > Arr[start] ) {
-    if ( Arr[start] <= target && target < Arr[mid] ) {
-      return searchRotated(Arr, target, start, mid - 1);  
+    if ( array[mid] >= array[start] ) {
+      if ( array[start] <= target && target < array[mid] ) {
+        end = mid - 1;  
+      } else {
+        start = mid + 1;    
+      }  
     } else {
-      return searchRotated(Arr, target, mid + 1, end);    
-    }  
-  } else if ( Arr[mid] < Arr[start] ) {
-    if ( Arr[mid] < target && target < Arr[end] ) {
-      return searchRotated(Arr, target, mid + 1, end);  
-    } else {
-      return searchRotated(Arr, target, start, mid - 1);    
-    }   
-  }	
-  return -1;
+      if ( array[mid] < target && target <= array[end] ) {
+        start = mid + 1;  
+      } else {
+        end = mid - 1;    
+      }   
+    }
+  }  	
+  return null;
 };
 
 var A = [15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14];
 
-console.log(searchRotated(A, 5, 0, 12));
+console.log(searchRotated(A, 15));
