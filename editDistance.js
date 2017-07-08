7488eba1d463deeccd341df8c51d6d96d3242f1a
Adam Watt <awattny@me.com>
2017-07-07 20:47:00 -0700

    re implement editDistance

--- editDistance.js
+++ editDistance.js
@@ -27,3 +27,22 @@ const editDistance = ( string1, string2 ) => {
 }; 
 
 console.log(editDistance('adceg', 'abcfg'));
+
+const editDist = ( str1, str2, m, n ) => {
+  if ( m === 0 ) {
+    return n;
+  }
+  if ( n === 0 ) {
+    return m;
+  }
+  if ( str1[m - 1] === str2[n - 1] ) {
+    return editDist( str1, str2, m - 1, n - 1);
+  }
+  return 1 + Math.min( editDist(str1, str2, m, n - 1),
+                       editDist(str1, str2, m - 1, n),
+                       editDist(str1, str2, m - 1, n - 1)
+                      );
+};
+
+// console.log(editDist('sunday', 'saturday', 6, 8));
+
