c486999015b51c3f8dc791af79f10cb329ed9ca7
Adam Watt <awattny@me.com>
2017-08-01 20:14:24 -0700

    reimplement segment string

--- segmentString.js
+++ segmentString.js
@@ -1,6 +1,6 @@
 const canSegmentString = (str, dict) => {
   const recurse = (str) => {
-    for ( var i = 1; i < str.length; i++ ) {
+    for ( var i = 1; i <= str.length; i++ ) {
       let first = str.slice(0, i);
       if ( dict.has(first)) {
         let second = str.slice(i);
@@ -17,24 +17,32 @@ const canSegmentString = (str, dict) => {
     }
     return false;
   };
-  const memoize = (func) => {
-    let memo = {};
-    return function(...args) {
-      //or let args = Array.prototype.slice.call(arguments);
-      if ( memo[args] ) {
-        return memo[args];
-      } else {
-        memo[args] = func.apply(null, args);
-        return memo[args];
-      }
-    };
-  };
-  let memoizedRecurse = memoize(recurse);
-  return memoizedRecurse(str);
+  return recurse(str);
 
 };
 
+const segmentString = ( dict, string ) => {
+  if ( string === '' ) {
+    return true;
+  }
+  for ( var i = 1; i <= string.length; i++ ) {
+    let firstWord = string.slice(0, i);
+    let restOfstring;
+    if ( dict.has(firstWord) ) {
+      restOfString = string.slice(i, string.length);
+      if ( segmentString(dict, restOfString) ) {
+        return true;
+      }
+      
+    }
+  }
+  return false;
+};
 
+const data = [['apple', true], ['pear', true], ['pier', true], ['pie', true]];
+const dict = new Map(data);
+console.log(canSegmentString('pearpierpie', dict));
+console.log(segmentString(dict, 'pearpierpie'));
 
 
 
