4909edf96df1f5ce497cb9fc4be591dd0d989063
Adam Watt <awattny@me.com>
2017-09-11 17:48:36 -0700

    implement array Max Consecutive Sum - code fights

---
+++ arrayMaxConsecutiveSum.js
@@ -0,0 +1,11 @@
+const arrayMaxConsecutiveSum = (inputArray) => {
+  let currentMax = 0;
+  let maxSoFar = -Infinity;
+  for ( var i = 0; i < inputArray.length; i++ ) {
+    let currentValue = inputArray[i];
+    currentMax += currentValue;
+    maxSoFar = Math.max(currentMax, maxSoFar);
+    currentMax = Math.max( 0, currentMax);
+  }
+  return maxSoFar;
+};
