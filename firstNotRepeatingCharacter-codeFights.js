a4197110ca7bf44b8c0ed9ca916e77635aadb0f5
Adam Watt <awattny@me.com>
2017-09-14 21:16:47 -0700

    minor fix

--- firstNotRepeatingCharacter-codeFights.js
+++ firstNotRepeatingCharacter-codeFights.js
@@ -1,11 +1,13 @@
-// Given a string s, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+// Given a string s, find and return the first instance of a non-repeating character in it. 
+//If there is no such character, return '_'.
 
 // Example
 
 // For s = "abacabad", the output should be
 // firstNotRepeatingCharacter(s) = 'c'.
 
-// There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+// There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it 
+//appears in the string first.
 
 // For s = "abacabaabacaba", the output should be
 // firstNotRepeatingCharacter(s) = '_'.
